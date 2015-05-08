c32f06bce89466422f5fef8c91a7f12e7e7f6c2f
bundai223 <bundai223@gmail.com>
2015-05-08 13:05:27 +0900

    os毎の設定を分割

---
+++ 000_common/000_common_osx.zsh
@@ -0,0 +1,26 @@
+refresh_memory()
+{
+    du -sx / &> /dev/null & sleep 25 && kill $!
+}
+
+install_font() {
+    cp $1 ~/Library/Fonts/
+}
+
+show_allfile_on_finder() {
+    flag="$1"
+    if [ "on" = $flag ]; then
+        echo "Enable show all files on finder."
+        defaults write com.apple.finder AppleShowAllFiles -boolean true
+        killall Finder
+    elif [ "off" = $flag ]; then
+        echo "Reset show all files on finder."
+        defaults delete com.apple.finder AppleShowAllFiles
+        killall Finder
+    else
+        echo "usage:"
+        echo "    show_allfile_on_finder [on|off]"
+    fi
+}
+
+alias compile_plantuml="java -jar ~/local/bin/plantuml.jar  -charset UTF-8 -tsvg -nbthread auto $<"
