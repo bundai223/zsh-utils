f139254943f870e17d909ed7153d14c8110ae825
bundai223 <bundai223@gmail.com>
2015-10-06 11:19:20 +0900

    ghq_path: multi path suport

--- 100_tools/150_ghq.zsh
+++ 100_tools/150_ghq.zsh
@@ -2,6 +2,5 @@ ghq_path()
 {
   repo=${1}
   root_path=$(ghq root ${repo})
-  sub_path=$(ghq list ${repo})
-  echo ${root_path}/${sub_path}
+  ghq list ${repo} | xargs -Isub_path echo ${root_path}/sub_path
 }
