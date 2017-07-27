e132dcb8ab42842bd1690515ff63c7e4c3448d74
bundai223 <bundai223@gmail.com>
2017-07-27 11:56:05 +0900

    add node

--- 200_lang/200_python.zsh
+++ 200_lang/200_python.zsh
@@ -9,3 +9,15 @@ py3_help() {
 }
 
 
+# pip zsh completion start
+function _pip3_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip3_completion pip3
+# pip zsh completion end
+
