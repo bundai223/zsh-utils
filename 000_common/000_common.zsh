b16fb6ed6743cdfaea348336e51b30bcf71963ee
bundai223 <bundai223@gmail.com>
2017-08-17 16:42:10 +0900

    fix indent

--- 000_common/000_common.zsh
+++ 000_common/000_common.zsh
@@ -16,54 +16,60 @@ sum() {
 
 
 ls_sshhost() {
-    awk '
-        tolower($1)=="host" {
-            for (i=2; i<=NF; i++) {
-                if ($i !~ "[*?]") {
-                    print $i
-                }
-            }
-        }
-    ' ~/.ssh/config | sort
+  awk '
+  tolower($1)=="host" {
+  for (i=2; i<=NF; i++) {
+    if ($i !~ "[*?]") {
+      print $i
+    }
+  }
+}
+' ~/.ssh/config | sort
 }
 
 listup_ip() {
-    LANG=C ifconfig | grep 'inet ' | awk '{print $2;}' | cut -d: -f2
-    #LANG=C ifconfig | grep 'inet addr' | awk '{print $2;}' | cut -d: -f2
+  LANG=C ifconfig | grep 'inet ' | awk '{print $2;}' | cut -d: -f2
+  #LANG=C ifconfig | grep 'inet addr' | awk '{print $2;}' | cut -d: -f2
 }
 
 os_version() {
-    VERSION_FILE_ARRAY=(\
-        '/etc/redhat-release' \
-        '/etc/fedora-release' \
-        '/etc/debian_version' \
-        '/etc/turbolinux-release' \
-        '/etc/SuSE-release' \
-        '/etc/mandriva-release' \
-        '/etc/vine-release' \
-        '/etc/issue' \
+  VERSION_FILE_ARRAY=(\
+    '/etc/redhat-release' \
+    '/etc/fedora-release' \
+    '/etc/debian_version' \
+    '/etc/turbolinux-release' \
+    '/etc/SuSE-release' \
+    '/etc/mandriva-release' \
+    '/etc/vine-release' \
+    '/etc/issue' \
     )
 
-    if [ 'Darwin' = $(uname) ]; then
-        sw_vers
-    else
-        for file in $VERSION_FILE_ARRAY; do
-            if [ -e $file ]; then
-                cat $file; break
-            fi
-        done
-    fi
+  if [ 'Darwin' = $(uname) ]; then
+    sw_vers
+  else
+    for file in $VERSION_FILE_ARRAY; do
+      if [ -e $file ]; then
+        cat $file; break
+      fi
+    done
+  fi
 }
 
 # tmux上でsshした際に対象のホスト名に応じてpaneの色を変える
 ssh() {
   # tmux起動時
   if [[ -n $(printenv TMUX) ]] ; then
+    h=$1
     # 接続先ホスト名に応じて背景色を切り替え
-    if [[ `echo $1 | grep '\.production\.'` ]] ; then
+    if [[ `echo $h | grep 'prod-'` ]] ; then
+      tmux select-pane -P 'fg=white'
       tmux select-pane -P 'bg=blue'
-    elif [[ `echo $1 | grep '\.dev\.'` ]] ; then
-      tmux select-pane -P 'bg=red'
+    elif [[ `echo $h | grep 'dev-'` ]] ; then
+      tmux select-pane -P 'fg=white'
+      tmux select-pane -P 'bg=blue'
+    elif [[ `echo $h | sed 's/^.*@//g' | grep '[0-9.]*'` ]] ; then
+      tmux select-pane -P 'fg=white'
+      tmux select-pane -P 'bg=green'
     fi
     # 通常通りssh続行
     command ssh $@
